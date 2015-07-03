eb6ae94201d23ed23085851525e724b41c9a0860
Diogo Doreto <diogo.doreto@gmail.com>
2015-07-03 16:42:11 -0300

    First batch of modifications

---
+++ system/colors.zsh
@@ -0,0 +1,4 @@
+# Base16 Shell
+BASE16_SHELL="$HOME/.config/base16-shell/base16-default.dark.sh"
+[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
+
---
+++ zsh/prezto.zsh
@@ -0,0 +1,8 @@
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}" 2> /dev/null
+done
+
+# Source Prezto.
+if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
+  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+fi
