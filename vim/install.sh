22b0523f375168fbd59c411c016576c67e173351
Diogo Doreto <diogo.doreto@gmail.com>
2015-07-04 11:38:04 -0300

    VIM!

---
+++ vim/install.sh
@@ -0,0 +1,8 @@
+# Vundle: Vim plugin manager
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+
+# YouCompleteMe: This plugin takes the longest time to download all its deps. So, let's do the downloading as a separate step.
+git clone https://github.com/Valloric/YouCompleteMe.git $HOME/.vim/bundle/YouCompleteMe
+cd $HOME/.vim/bundle/YouCompleteMe && git submodule update --init --recursive \
+   && ./install.sh --gocode-completer
+
