dd526ab5a9581bf22e8d0d496efd1a0cd97da4cd
Diogo Doreto <diogo.doreto@gmail.com>
2015-08-25 11:22:44 -0300

    Use new YouCompleteMe install script

--- vim/install.sh
+++ vim/install.sh
@@ -4,7 +4,7 @@ git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 # YouCompleteMe: This plugin takes the longest time to download all its deps. So, let's do the downloading as a separate step.
 git clone https://github.com/Valloric/YouCompleteMe.git $HOME/.vim/bundle/YouCompleteMe
 cd $HOME/.vim/bundle/YouCompleteMe && git submodule update --init --recursive \
-   && ./install.sh --gocode-completer
+   && ./install.py --gocode-completer
 
 # Install and make vimproc (needed for typescript's plugin tsuquyomi)
 git clone https://github.com/Shougo/vimproc.vim.git $HOME/.vim/bundle/vimproc.vim
