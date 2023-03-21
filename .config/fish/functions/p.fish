7b4f0f577860380295628ba4c4ecc4f2e113a4a0
Diogo Doreto <diogo@doreto.com.br>
2023-03-21 16:35:46 +0100

    add ranger config to project list

--- .config/fish/functions/p.fish
+++ .config/fish/functions/p.fish
@@ -1,5 +1,5 @@
 function p
-  set -l dirs ~/projects/* ~/.config/nvim ~/.config/astronvim ~/.config/fish ~/.config/wezterm
+  set -l dirs ~/projects/* ~/.config/nvim ~/.config/astronvim ~/.config/fish ~/.config/wezterm ~/.config/ranger
   set -l choice (string join \n $dirs | fzf)
   if test -n "$choice"
     cd "$choice"
