f264f83312bbc978a70509d9fdea69c3c593c258
Diogo Doreto <diogo@doreto.com.br>
2023-03-30 13:27:18 +0200

    nvim: add file path to winbar

--- .config/astronvim/lua/user/plugins/heirline.lua
+++ .config/astronvim/lua/user/plugins/heirline.lua
@@ -1,6 +1,7 @@
 return {
   "rebelot/heirline.nvim",
   opts = function(_, baseConfig)
+    local status = require("astronvim.utils.status")
     local statusLine = baseConfig.statusline
 
     -- remove vim mode from beginning and end
@@ -11,6 +12,18 @@ return {
     statusLine[#statusLine] = ruler
     -- print cwd dir
     table.insert(statusLine, 1, { provider = vim.fs.basename(vim.fn.getcwd()) .. "/ " })
+
+    local winbar = baseConfig.winbar
+    winbar[#winbar] = {
+      status.component.breadcrumbs({ hl = status.hl.get_attributes("winbar", true) }),
+      status.component.fill(),
+      {
+        provider = function()
+          return vim.fn.expand("%")
+        end,
+        update = "BufEnter",
+      },
+    }
 
     return baseConfig
   end,
