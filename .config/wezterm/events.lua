53c6c97a0f789213de60c9d556b5307b8c87cf4e
Diogo Doreto <diogo@doreto.com.br>
2023-04-12 09:09:02 +0200

    wezterm: tab_bar follows current color scheme

--- .config/wezterm/events.lua
+++ .config/wezterm/events.lua
@@ -1,43 +1,35 @@
 local wezterm = require("wezterm")
+local colorschemes = wezterm.get_builtin_color_schemes()
 
 local function get_current_working_dir(tab)
   local current_dir = tab.active_pane.current_working_dir
   local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
 
-  return current_dir == HOME_DIR and "~"
-      or string.format("%s", string.gsub(current_dir, "(.*[/\\])(.*)", "%2"))
+  return current_dir == HOME_DIR and "~" or string.format("%s", string.gsub(current_dir, "(.*[/\\])(.*)", "%2"))
 end
 
-wezterm.on(
-  'format-tab-title',
-  function(tab, tabs, panes, config, hover, max_width)
-    local edge_background = "#0c0c0c"
-    local background = '#252525'
-    local foreground = '#b6b8bb'
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local current_scheme = colorschemes[config.color_scheme]
+  local edge_background = current_scheme.tab_bar.background
 
-    if tab.is_active then
-      background = '#7b7c7e'
-      foreground = '#161616'
-    end
-    local edge_foreground = background
+  local tab_scheme = current_scheme.tab_bar.inactive_tab
+  if tab.is_active then tab_scheme = current_scheme.tab_bar.active_tab end
+  local edge_foreground = tab_scheme.bg_color
 
-    local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
-    local cwd = get_current_working_dir(tab)
+  local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
+  local cwd = get_current_working_dir(tab)
 
-    -- ensure that the titles fit in the available space,
-    -- and that we have room for the edges.
-    local title = wezterm.truncate_right(
-      " " .. (tab.tab_index + 1) .. ": " .. process_name .. " " .. cwd,
-      max_width - 2
-    ) .. " "
+  -- ensure that the titles fit in the available space,
+  -- and that we have room for the edges.
+  local title = wezterm.truncate_right(" " .. (tab.tab_index + 1) .. ": " .. process_name .. " " .. cwd, max_width - 2)
+      .. " "
 
-    return {
-      { Background = { Color = background } },
-      { Foreground = { Color = foreground } },
-      { Text = title },
-      { Background = { Color = edge_background } },
-      { Foreground = { Color = edge_foreground } },
-      { Text = " " },
-    }
-  end
-)
+  return {
+    { Background = { Color = tab_scheme.bg_color } },
+    { Foreground = { Color = tab_scheme.fg_color } },
+    { Text = title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = " " },
+  }
+end)
