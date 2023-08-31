84b46ab484f15e6a1e1a86759c35786d66660c4a
Diogo Doreto <diogo@doreto.com.br>
2023-08-31 16:35:45 +0200

    nvim: move lsp config to init.lua

---
+++ .config/astronvim/lua/user/init.lua
@@ -0,0 +1,13 @@
+return {
+  lsp = {
+    setup_handlers = {
+      tsserver = function(_, opts) require("typescript").setup({ server = opts }) end,
+    },
+    formatting = {
+      disabled = {
+        "lua_ls",
+        "tsserver",
+      },
+    },
+  },
+}
