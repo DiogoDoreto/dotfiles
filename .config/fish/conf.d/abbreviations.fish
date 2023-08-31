64ddd556ad103cc7b886e85b0fdf3479e32ecacf
Diogo Doreto <diogo@doreto.com.br>
2023-08-31 16:36:35 +0200

    fish: lld to list directories

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -4,6 +4,7 @@ if status is-interactive
   abbr --add r ranger
   abbr --add ll exa --icons --long
   abbr --add la exa --icons --long --all
+  abbr --add lld exa --icons --long --only-dirs
 
   function multicd
     echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
