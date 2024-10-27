f20742055c8f6a4dd908fecbbc89f748406446b6
Diogo Doreto <diogo@doreto.com.br>
2024-10-27 23:53:24 +0100

    i3: i3status customization

--- programs/i3.nix
+++ programs/i3.nix
@@ -23,15 +23,16 @@ in
       windowManager.i3 = {
         enable = true;
         config = {
+          modifier = "Mod4"; # Windows key
+          terminal = cfg.terminal;
+          gaps.inner = 10;
+          workspaceAutoBackAndForth = true;
           menu = strings.concatStringsSep " " [
             (lib.getExe pkgs.rofi)
             "-show combi"
             "-modes combi"
             "-combi-modes \"window#drun#Power:rofi-power-menu --choices=shutdown/reboot --confirm=logout\""
           ];
-          modifier = "Mod4"; # Windows key
-          terminal = cfg.terminal;
-          gaps.inner = 10;
           keybindings = let
             modifier = config.xsession.windowManager.i3.config.modifier;
           in lib.mkOptionDefault {
@@ -43,8 +44,101 @@ in
             "Ctrl+Shift+Mod1+t" = "exec ${cfg.terminal}";
             "${modifier}+period" = "exec rofimoji";
           };
+          bars = [{
+            mode = "dock";
+            hiddenState = "hide";
+            position = "bottom";
+            workspaceButtons = true;
+            workspaceNumbers = true;
+            statusCommand = "${pkgs.i3status}/bin/i3status";
+            fonts = {
+              names = [ "VictorMono Nerd Font Propo" ];
+              size = 10.0;
+            };
+            trayOutput = "primary";
+            colors = {
+              background = "#000000";
+              statusline = "#ffffff";
+              separator = "#666666";
+              focusedWorkspace = {
+                border = "#4c7899";
+                background = "#285577";
+                text = "#ffffff";
+              };
+              activeWorkspace = {
+                border = "#333333";
+                background = "#5f676a";
+                text = "#ffffff";
+              };
+              inactiveWorkspace = {
+                border = "#333333";
+                background = "#222222";
+                text = "#888888";
+              };
+              urgentWorkspace = {
+                border = "#2f343a";
+                background = "#900000";
+                text = "#ffffff";
+              };
+              bindingMode = {
+                border = "#2f343a";
+                background = "#900000";
+                text = "#ffffff";
+              };
+            };
+          }];
         };
       };
+    };
+
+    home.file = {
+      ".config/i3status/config".text = ''
+        general {
+          colors = true
+          interval = 5
+        }
+
+        order += "wireless _first_"
+        order += "ethernet _first_"
+        order += "disk /"
+        order += "load"
+        order += "memory"
+        order += "battery all"
+        order += "tztime local"
+
+        wireless _first_ {
+          format_up = " (%quality at %essid) %ip"
+          format_down = "󰖪"
+        }
+
+        ethernet _first_ {
+          format_up = " %ip (%speed)"
+          format_down = "󰲜"
+        }
+
+        battery all {
+          format = "%status %percentage %remaining"
+          format_down = "󱉞"
+        }
+
+        disk "/" {
+          format = " %avail"
+        }
+
+        load {
+          format = " %1min"
+        }
+
+        memory {
+          format = " %used | %available"
+          threshold_degraded = "1G"
+          format_degraded = "MEMORY < %available"
+        }
+
+        tztime local {
+          format = "󰸘 %Y-%m-%d %H:%M:%S"
+        }
+      '';
     };
 
     home.packages = with pkgs; [
