50b25abb636b2ef99dfa0aac500bfa874e5291d6
Diogo Doreto <diogo@doreto.com.br>
2024-10-30 21:00:26 +0100

    add git initialization script

--- init-linux.sh
+++ init-linux.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+# init git
+read -p "git config user.email " email
+git config --file ~/.config/git/config.private user.email $email
+
+# init doom emacs
 chmod -R 775 ~/.local/share/doomemacs/cache
 chmod -R 775 ~/.local/share/doomemacs/profiles
 chmod -R 775 ~/.local/share/doomemacs/state
