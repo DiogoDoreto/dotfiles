033b06ddd08439f5d8ab840bcbae91f174726dc9
Diogo Doreto <diogo@doreto.com.br>
2025-01-16 00:17:16 +0100

    wsl-host: add nvidia

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -8,9 +8,16 @@
   home = {
     packages = with pkgs; [
       neovim
+      ollama-cuda
     ];
 
     stateVersion = "24.11";
+  };
+
+  programs = {
+    starship.settings = {
+      username.show_always = true;
+    };
   };
 
   dog.programs = {
