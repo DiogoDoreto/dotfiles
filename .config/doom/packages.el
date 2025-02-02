501b46b3fa103284a6dc020c2352d9a95fec6d1c
Diogo Doreto <diogo@doreto.com.br>
2025-02-02 02:30:55 +0100

    add gptel emacs package

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -19,6 +19,8 @@
 
 (package! ct) ;; Color Tools
 
+(package! gptel)
+
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
