6e4f2e1bdd9653d5b74089dfa4e90c5ea6d7eb1b
Diogo Doreto <diogo@doreto.com.br>
2025-02-02 02:33:04 +0100

    use latest aider-chat version

--- hosts/chungus/ai.nix
+++ hosts/chungus/ai.nix
@@ -9,6 +9,8 @@
       #   torch = python312Packages.torchWithCuda;
       # })
 
+      (pkgs-unstable.callPackage ./aider-chat.nix {})
+
       (comfyuiPackages.comfyui.override {
         extensions = [
           comfyuiPackages.extensions.acly-inpaint
@@ -19,9 +21,7 @@
         commandLineArgs = [ "--preview-method" "auto" ];
       })
       comfyuiPackages.krita-with-extensions
-    ] ++ (with pkgs-unstable; [
-      aider-chat
-    ]);
+    ];
   };
 
   # Ollama
@@ -37,13 +37,43 @@
   home.sessionVariables = {
     OLLAMA_API_BASE = "http://127.0.0.1:11434";
   };
-  home.file.".aider.conf.yml" = {
-    text = builtins.toJSON {
-      editor = "emacs";
-      model = "ollama_chat/deepseek-r1:32b";
-      editor-model = "ollama_chat/qwen2.5-coder:32b";
-      dark-mode = true;
-      analytics-disable = true;
+  home.file = {
+    ".aider.conf.yml" = {
+      text = builtins.toJSON {
+        architect = true;
+        model = "ollama_chat/deepseek-r1:32b";
+        editor-model = "ollama_chat/qwen2.5-coder:32b";
+        dark-mode = true;
+        attribute-author = false;
+        attribute-committer = false;
+        alias = [
+          "r1:ollama_chat/deepseek-r1:32b"
+          "qwen:ollama_chat/qwen2.5-coder:32b"
+        ];
+      };
+    };
+    ".aider.model.settings.yml" = {
+      text = builtins.toJSON [
+        {
+          name = "ollama_chat/deepseek-r1:32b";
+          extra_params = {
+            num_ctx = 64000;
+          };
+          remove_reasoning = "think";
+          use_temperature = false;
+          use_repo_map = true;
+          edit_format = "diff";
+        }
+        {
+          name = "ollama_chat/qwen2.5-coder:32b";
+          extra_params = {
+            # num_ctx = 8192;
+            num_ctx = 32768;
+          };
+          use_repo_map = true;
+          edit_format = "diff";
+        }
+      ];
     };
   };
 
