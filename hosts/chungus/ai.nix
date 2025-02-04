972a89e1004c8532382005680467f861334d1476
Diogo Doreto <diogo@doreto.com.br>
2025-02-04 22:24:38 +0100

    improve aider's context size to fit in gpu only

--- hosts/chungus/ai.nix
+++ hosts/chungus/ai.nix
@@ -48,31 +48,43 @@
         attribute-committer = false;
         alias = [
           "r1:ollama_chat/deepseek-r1:32b"
+          "r1-small:ollama_chat/deepseek-r1:14b"
           "qwen:ollama_chat/qwen2.5-coder:32b"
+          "qwen-small:ollama_chat/qwen2.5-coder:14b"
         ];
       };
     };
-    ".aider.model.settings.yml" = {
-      text = builtins.toJSON [
-        {
-          name = "ollama_chat/deepseek-r1:32b";
-          extra_params = {
-            num_ctx = 64000;
-          };
-          remove_reasoning = "think";
+    ".aider.model.settings.yml" = let
+        deepseek = {
+          name = "ollama_chat/deepseek-r1";
+          # remove_reasoning = "think";
           use_temperature = false;
           use_repo_map = true;
           edit_format = "diff";
-        }
-        {
-          name = "ollama_chat/qwen2.5-coder:32b";
-          extra_params = {
-            # num_ctx = 8192;
-            num_ctx = 32768;
-          };
+        };
+        qwen = {
+          name = "ollama_chat/qwen2.5-coder";
           use_repo_map = true;
           edit_format = "diff";
-        }
+        };
+      in {
+      text = builtins.toJSON [
+        (deepseek // {
+          name = deepseek.name + ":14b";
+          extra_params = { num_ctx = 48000; };
+        })
+        (deepseek // {
+          name = deepseek.name + ":32b";
+          extra_params = { num_ctx = 10000; };
+        })
+        (qwen // {
+          name = qwen.name + ":14b";
+          extra_params = { num_ctx = 32768; };
+        })
+        (qwen // {
+          name = qwen.name + ":32b";
+          extra_params = { num_ctx = 10000; };
+        })
       ];
     };
   };
