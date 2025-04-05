b7f5ccbacad8c309e67f1149a7198f4787c566cc
Diogo Doreto <diogo@doreto.com.br>
2025-04-05 23:43:21 +0200

    host(mini): add openvpn

--- hosts/mini/configuration.nix
+++ hosts/mini/configuration.nix
@@ -120,6 +120,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    openvpn
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
