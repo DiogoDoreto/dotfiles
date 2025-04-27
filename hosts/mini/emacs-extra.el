49c0ff695984e27563fda518f0a4252972681279
Diogo Doreto <diogo@doreto.com.br>
2025-04-28 00:15:34 +0200

    gptel tools

--- hosts/mini/emacs-extra.el
+++ hosts/mini/emacs-extra.el
@@ -1,9 +1,9 @@
 ;;; -*- lexical-binding: t; -*-
 (after! gptel
-  (setq gptel-model 'qwen2.5-coder:32b
-        gptel-backend (gptel-make-ollama "Ollama"
-                        :host "chungus.home:11434"
-                        :stream t
-                        :models '(qwen2.5-coder:32b deepseek-r1:32b qwq))))
+  (gptel-make-ollama "Ollama"
+    :host "chungus.home:11434"
+    :stream t
+    :models '(qwen2.5-coder:32b deepseek-r1:32b qwq)))
 
-(add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+(after! tramp
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
