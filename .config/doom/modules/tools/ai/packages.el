e37602ffe89c189efe7275782071d856fe1d5d51
Diogo Doreto <diogo@doreto.com.br>
2025-05-06 11:06:44 +0200

    emacs: update ai packages

--- .config/doom/modules/tools/ai/packages.el
+++ .config/doom/modules/tools/ai/packages.el
@@ -1,5 +1,5 @@
 ;; -*- lexical-binding: t; no-byte-compile: t; -*-
 
-(package! gptel :pin "0c5a95d527b22380112356cda1719fc43de893bd")
+(package! gptel :pin "1aa5f1c10df34aeb08f677cd4aa4ef1fc12e87df")
 
-(package! copilot :pin "229d8544834a8d446ec9747102ad160280a65620")
+(package! copilot :pin "782970051c4ad569140fcc24ff6eacb0f0b40cef")
