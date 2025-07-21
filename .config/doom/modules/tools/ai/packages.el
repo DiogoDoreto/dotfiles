7da8aaffa4a00f62783db42996a3a86b2f62b515
Diogo Doreto <diogo@doreto.com.br>
2025-07-21 09:37:49 +0200

    chore(emacs): update gptel

--- .config/doom/modules/tools/ai/packages.el
+++ .config/doom/modules/tools/ai/packages.el
@@ -1,6 +1,6 @@
 ;; -*- lexical-binding: t; no-byte-compile: t; -*-
 
-(package! gptel :pin "e9d92423d138e6f217a494ac4b28fe68d349233a")
+(package! gptel :pin "7218aedd6f8294af5be876c0c18a733863156b7b")
 
 (package! copilot :pin "fe3f51b636dea1c9ac55a0d5dc5d7df02dcbaa48")
 
