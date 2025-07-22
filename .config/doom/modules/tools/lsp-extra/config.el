d2f0e545c6c3461ff1072c823b8df7abf1dcd9f3
Diogo Doreto <diogo@doreto.com.br>
2025-07-22 10:46:17 +0200

    feat(emacs): improve LSP code action selection and auto-execution
    Refactor `lsp-extra-show-actions-at-point` to better handle
    auto-execution of common code actions and add explicit universal
    argument (ARG) handling. Now, commonly repeated actions such as "Remove
    'async'" and "Remove unnecessary `await`" are considered for
    auto-execution if present. If no matching auto-run action is found, or
    if a universal argument is provided, the function offers an interactive
    popup menu for choosing from all available code actions.

--- .config/doom/modules/tools/lsp-extra/config.el
+++ .config/doom/modules/tools/lsp-extra/config.el
@@ -20,21 +20,35 @@
     (and action-title
          (or (string= action-title "Add async modifier to containing function")
              (s-starts-with? "Remove unused declaration for:" action-title)
-             (s-starts-with? "Remove import from " action-title))))
+             (s-starts-with? "Remove import from " action-title)
+             (s-starts-with? "Remove 'async'" action-title)
+             (s-starts-with? "Remove unnecessary `await`" action-title))))
 
-  (defun lsp-extra-show-actions-at-point ()
-    (interactive)
+  (defun lsp-extra--run-code-action-with-title (action-title)
+    "Retrive an up-to-date list of code-actions and run the one that matches the
+ACTION-TITLE"
+    (let ((selected-action (seq-find (lambda (hs) (string= (lsp:code-action-title hs) action-title))
+                                     (lsp-code-actions-at-point))))
+      (if selected-action (lsp-execute-code-action selected-action)
+        (message "Could not find action: %s" action-title))))
+
+  (defun lsp-extra-show-actions-at-point (ARG)
+    "Display and optionally auto-execute LSP code actions at point.
+
+This command collects available code actions at the current point. By default,
+it will automatically execute a predefined action if one matches the criteria.
+If no matching action is found, or if a universal argument (ARG) is provided, a
+popup menu will list all available actions for selection."
+    (interactive "P")
     (let* ((action-list (lsp-code-actions-at-point))
-           (action-title-list (mapcar #'lsp-extra--code-action-to-popup-item action-list)))
-      (if (lsp-extra--should-auto-run-action (car action-title-list))
-          (lsp-execute-code-action (car action-list))
-        (if (not action-title-list) (message "No actions at point")
-          (let* ((selected-action-title (popup-menu* action-title-list :isearch t))
-                 (selected-action (seq-find (lambda (hs) (string= (lsp:code-action-title hs) selected-action-title))
-                                            (lsp-code-actions-at-point))))
-            (if selected-action (lsp-execute-code-action selected-action)
-              (message "Could not find action: %s" selected-action-title)))))))
-
+           (action-title-list (mapcar #'lsp-extra--code-action-to-popup-item action-list))
+           (auto-exec-action-title (and (not ARG) (seq-find #'lsp-extra--should-auto-run-action action-title-list))))
+      (cond
+       (auto-exec-action-title
+        (lsp-extra--run-code-action-with-title auto-exec-action-title))
+       (action-title-list
+        (lsp-extra--run-code-action-with-title (popup-menu* action-title-list :isearch t)))
+       (t (message "No actions at point")))))
 
   (map! :leader
         :desc "LSP actions popup" :nv "c ." #'lsp-extra-show-actions-at-point
