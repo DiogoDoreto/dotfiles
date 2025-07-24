ed4cff3ce8f24f5152a547d548312ccc0cfaf9d8
Diogo Doreto <diogo@doreto.com.br>
2025-07-24 17:36:44 +0200

    emacs: support eslint error messages in compilation-mode

--- .config/doom/config-javascript.el
+++ .config/doom/config-javascript.el
@@ -26,3 +26,26 @@
        :desc "Run test under cursor" "t" #'jest-test-run-at-point))
 
 (add-to-list '+lookup-provider-url-alist '("NPM" "https://www.npmjs.com/search?q=%s"))
+
+(after! compile
+  ;; eslint errors parsing for compilation-mode
+  ;; from https://github.com/Fuco1/compile-eslint/blob/master/compile-eslint.el
+
+  (defun compile-eslint--find-filename ()
+    "Find the filename for current error."
+    (save-match-data
+      (save-excursion
+        (when (re-search-backward (rx bol (group "/" (+ any)) eol))
+          (list (match-string 1))))))
+
+  (let ((form `(eslint
+                ,(rx-to-string
+                  '(and (group (group (+ digit)) ":" (group (+ digit)))
+                        (+ " ") (or "error" "warning")))
+                compile-eslint--find-filename
+                2 3 2 1)))
+    (if (assq 'eslint compilation-error-regexp-alist-alist)
+        (setf (cdr (assq 'eslint compilation-error-regexp-alist-alist)) (cdr form))
+      (push form compilation-error-regexp-alist-alist)))
+
+  (push 'eslint compilation-error-regexp-alist))
