2cb783bfc9c4d660cf6c60dd7ad4b2e69a263160
Diogo Doreto <diogo@doreto.com.br>
2025-08-11 12:55:22 +0200

    emacs: update gptel

--- .config/doom/modules/tools/ai/packages.el
+++ .config/doom/modules/tools/ai/packages.el
@@ -1,6 +1,6 @@
 ;; -*- lexical-binding: t; no-byte-compile: t; -*-
 
-(package! gptel :pin "5897d07a3aa164243ee42f67e4f7eb2bab0748d9")
+(package! gptel :pin "3fe5e08ddd8b1bc6f1eee8a9c6548bf7a029f55e")
 
 (package! copilot :pin "fe3f51b636dea1c9ac55a0d5dc5d7df02dcbaa48")
 
