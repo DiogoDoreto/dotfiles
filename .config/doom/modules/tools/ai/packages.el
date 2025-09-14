b18ab8d8e7c82a741b51d17f115a583fa0dd29bd
Diogo Doreto <diogo@doreto.com.br>
2025-09-14 13:46:05 +0200

    lapdog: ❄️

--- .config/doom/modules/tools/ai/packages.el
+++ .config/doom/modules/tools/ai/packages.el
@@ -1,6 +1,6 @@
 ;; -*- lexical-binding: t; no-byte-compile: t; -*-
 
-(package! gptel :pin "7bb353a582d11ce210bb0a5b77c53de564c28e67")
+(package! gptel :pin "f66e44368fd085f958f5d34cdbcbfb649abfc5eb")
 
 (package! copilot :pin "4f51b3c21c42756d09ee17011201ea7d6e18ff69")
 
