37baebb2b0cc8ff06995e0105db48d92745a654e
Diogo Doreto <diogo@doreto.com.br>
2025-09-14 23:02:01 +0200

    emacs: add interactive Nix package search and details viewer for Emacs
    This module introduces helper functions for searching Nix packages
    directly from Emacs using the NixOS online package search API. It
    provides an interactive command to search for packages, displays results
    in a tabulated list with columns for name, version, description,
    programs, and platforms, and allows users to view detailed information
    for each package in a dedicated buffer.

---
+++ .config/doom/dd/dd-nix.el
@@ -0,0 +1,146 @@
+;;; dd-nix.el --- Nix helpers -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Diogo Doreto
+;;
+;;; Commentary:
+;;
+;;  Nix helpers
+;;
+;;; Code:
+
+(require 'json)
+(require 'tabulated-list)
+(require 'url)
+
+(defvar-local dd-nix-search--package-alist nil
+  "stores (ID . full-package-info) for the search results buffer")
+
+(defun dd-nix--search-json-payload (query)
+  "Construct the JSON payload for the Nix search API using QUERY."
+  (json-encode
+   `((from . 0)
+     (size . 30)
+     (query
+      (bool
+       (filter ((term (type (value . "package")
+                            (_name . "filter_packages")))))
+       (must ((dis_max
+               (tie_breaker . 0.7)
+               (queries ((multi_match
+                          (type . "cross_fields")
+                          (query . ,query)
+                          (analyzer . "whitespace")
+                          (auto_generate_synonyms_phrase_query . :false)
+                          (operator . "and")
+                          (_name . "multi_match")
+                          (fields . ("package_attr_name^9"
+                                     "package_attr_name.*^5.3999999999999995"
+                                     "package_programs^9"
+                                     "package_programs.*^5.3999999999999995"
+                                     "package_pname^6"
+                                     "package_pname.*^3.5999999999999996"
+                                     "package_description^1.3"
+                                     "package_description.*^0.78"
+                                     "package_longDescription^1"
+                                     "package_longDescription.*^0.6"
+                                     "flake_name^0.5"
+                                     "flake_name.*^0.3"))))
+                        ((wildcard
+                          (package_attr_name
+                           (value . ,(concat "*" query "*"))
+                           (case_insensitive . t)))))))))))))
+
+(defun dd-nix-search (query)
+  "Search for Nix packages via https://search.nixos.org using QUERY."
+  (interactive (list (read-string "Nix search query: ")))
+  (let* ((url-request-method "POST")
+         (url-request-extra-headers '(("Content-Type" . "application/json")
+                                      ("Authorization" . "Basic YVdWU0FMWHBadjpYOGdQSG56TDUyd0ZFZWt1eHNmUTljU2g=")))
+         (url-request-data (dd-nix--search-json-payload query))
+         (api-url "https://search.nixos.org/backend/latest-44-nixos-unstable/_search")
+         (buf (generate-new-buffer "*Nix Package Search*")))
+    (message "Searching Nix packages for: %s..." query)
+    (url-retrieve
+     api-url
+     (lambda (_status)
+       (goto-char (point-min))
+       (re-search-forward "\n\n" nil t) ; skip headers
+       (let* ((json-object-type 'alist)
+              (json-array-type 'list)
+              (json-key-type 'symbol)
+              (data (json-read)))
+         (let* ((hits (alist-get 'hits (alist-get 'hits data)))
+                (package-alist
+                 (mapcar (lambda (hit)
+                           (let* ((src (alist-get '_source hit))
+                                  (attr-name (or (alist-get 'package_attr_name src) "")))
+                             (cons attr-name src)))
+                         hits))
+                (table
+                 (mapcar
+                  (lambda (hit)
+                    (let* ((src (alist-get '_source hit))
+                           (attr-name (or (alist-get 'package_attr_name src) ""))
+                           (version (or (alist-get 'package_pversion src) ""))
+                           (desc (or (alist-get 'package_description src) ""))
+                           (programs (string-join (or (alist-get 'package_programs src) '()) ", "))
+                           (platforms (string-join (or (alist-get 'package_platforms src) '()) ", ")))
+                      (list attr-name
+                            (vector attr-name version desc programs platforms))))
+                  hits)))
+           (with-current-buffer buf
+             (setq buffer-read-only nil)
+             (erase-buffer)
+             (dd-nix-search-results-mode)
+             (setq dd-nix-search--package-alist package-alist)
+             (setq tabulated-list-entries table)
+             (tabulated-list-init-header)
+             (tabulated-list-print)
+             (goto-char (point-min))
+             (setq buffer-read-only t)
+             (pop-to-buffer buf)))))
+     nil t)))
+
+(defun dd-nix-search--show-details ()
+  "Show full information about the currently selected Nix package."
+  (interactive)
+  (let* ((id (tabulated-list-get-id))
+         (pkg-src (when (boundp 'dd-nix-search--package-alist)
+                    (cdr (assoc id dd-nix-search--package-alist)))))
+    (if pkg-src
+        (let ((buf (generate-new-buffer (format "*Nix Package: %s*" (or (alist-get 'package_attr_name pkg-src) id)))))
+          (with-current-buffer buf
+            (setq buffer-read-only nil)
+            (erase-buffer)
+            (insert (format "Nix Package details for: %s\n\n" (or (alist-get 'package_attr_name pkg-src) id)))
+            (dolist (field pkg-src)
+              (let ((k (car field))
+                    (v (cdr field)))
+                (when v
+                  (insert (format "%20s: %s\n"
+                                  (symbol-name k)
+                                  (if (and (listp v) (not (stringp v)))
+                                      (mapconcat (lambda (item) (prin1-to-string item t)) v ", ")
+                                    (prin1-to-string v t)))))))
+            (goto-char (point-min))
+            (view-mode 1)
+            (goto-address-mode 1)
+            (setq buffer-read-only t)
+            (pop-to-buffer buf)))
+      (message "No details available for this package."))))
+
+(define-derived-mode dd-nix-search-results-mode tabulated-list-mode "Nix Search Results"
+  "Major mode for displaying Nix package search results."
+  (setq tabulated-list-format [("Name" 25 t)
+                               ("Version" 10 t)
+                               ("Description" 35 t)
+                               ("Programs" 20 t)
+                               ("Platforms" 25 t)])
+  (setq tabulated-list-padding 2)
+  (setq tabulated-list-sort-key (cons "Name" nil))
+  (tabulated-list-init-header)
+  (define-key dd-nix-search-results-mode-map (kbd "RET") #'dd-nix-search--show-details)
+  (evil-local-set-key 'normal (kbd "RET") #'dd-nix-search--show-details))
+
+(provide 'dd-nix)
+;;; dd-nix.el ends here
