c13b0c5d35bf7ecd97bb48e5553c7f1c2a92fdcd
Diogo Doreto <diogo@doreto.com.br>
2025-10-05 21:18:45 +0200

    emacs: update package pins for gptel, mcp, and aidermacs

--- .config/doom/modules/tools/ai/packages.el
+++ .config/doom/modules/tools/ai/packages.el
@@ -1,9 +1,9 @@
 ;; -*- lexical-binding: t; no-byte-compile: t; -*-
 
-(package! gptel :pin "33363745eae0819c58a0a48f84a0474789f2c9e5")
+(package! gptel :pin "3cf7bd0c850f6f72249d08d3f749dd4d04efa0f9")
 
-(package! mcp :pin "826ce208384940ed1f2e21184308f61ab0c8f477")
+(package! mcp :pin "b9782bb80027c13a84ae3ffff1e363a674ed6cf1")
 
 (package! copilot :pin "6a2ad80489b8a0d021df95293eb7ac370aea140b")
 
-(package! aidermacs :pin "d944f430eae936de2a9aa8121c7eaca2ca7be948")
+(package! aidermacs :pin "c6cb54d58aa5434ee39db77fde3945e078559f00")
