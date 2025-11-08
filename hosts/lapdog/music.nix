60d49fc121bcf5126562b4d662e6c20157e9aee3
Diogo Doreto <diogo@doreto.com.br>
2025-11-08 21:11:16 +0100

    lapdog: setup yabridge

--- hosts/lapdog/music.nix
+++ hosts/lapdog/music.nix
@@ -5,10 +5,20 @@
     ardour
     drumgizmo
     hydrogen
+
+    # wineWow
+    wineWowPackages.yabridge
+    yabridge
+    yabridgectl
   ];
 
   home.sessionVariables = {
     # DrumGizmo is a lv2 plugin
     LV2_PATH = "/etc/profiles/per-user/$USER/lib/lv2/";
   };
+
+  # needed for yabridgectl
+  home.sessionVariablesExtra = ''
+    export NIX_PROFILES="$NIX_PROFILES /etc/profiles/per-user/$USER"
+  '';
 }
