d2cb84d960888a469e0ae275bfefab823b400b17
Diogo Doreto <diogo@doreto.com.br>
2025-11-16 16:58:57 +0100

    mini: setup system backup with borg

--- hosts/mini/configuration.nix
+++ hosts/mini/configuration.nix
@@ -149,9 +149,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    borgbackup
     home-manager
     openvpn
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     #  wget
   ];
 
@@ -164,6 +165,57 @@
   # };
 
   # List services that you want to enable:
+
+  services.borgbackup.jobs.root = {
+    repo = "/root/test-bkp";
+    doInit = true;
+    compression = "auto,lzma";
+    startAt = "daily";
+    encryption = {
+      mode = "repokey-blake2";
+      passCommand = "cat /etc/secrets/borgbackup";
+    };
+    # TODO use when setting up BorgBase remote
+    # environment = {
+    #   BORG_RSH = "ssh -i /run/keys/id_ed25519_borgbase";
+    # };
+    paths = [
+      "/var/lib/"
+      "/var/lib/open-webui/data" # it's a symlink
+      "/home/dog/projects/home-assistant-config/config/"
+    ];
+    patterns = [
+      "+ /var/lib/dog/media/Books"
+
+      "+ /var/lib/freshrss/config.php"
+      "+ /var/lib/freshrss/users/*/config.php"
+      "+ /var/lib/freshrss/users/*/db.sqlite"
+
+      "- /var/lib/nextcloud/data/*/files_trashbin"
+      "+ /var/lib/nextcloud/data"
+      "+ /var/lib/nextcloud/config"
+
+      "- /var/lib/open-webui/data/audit.log"
+      "- /var/lib/open-webui/data/cache"
+      "+ /var/lib/open-webui/data"
+
+      "+ /var/lib/radarr/.config/Radarr/config.xml"
+      "+ /var/lib/radarr/.config/Radarr/radarr.db"
+
+      "+ /var/lib/sonarr/.config/NzbDrone/config.xml"
+      "+ /var/lib/sonarr/.config/NzbDrone/sonarr.db"
+
+      "+ /var/lib/jellyfin/config"
+      "+ /var/lib/jellyfin/data/jellyfin.db"
+      "+ /var/lib/jellyfin/root"
+
+      "+ /home/dog/projects/home-assistant-config/config/*.yaml"
+      "+ /home/dog/projects/home-assistant-config/config/home-assistant_v2.db"
+
+      # exclude everything that has not been explicitly added above
+      "- **"
+    ];
+  };
 
   services.nextcloud = {
     enable = true;
