388f6e9bc54d401f235e23716898c9aa76eaadec
Diogo Doreto <diogo@doreto.com.br>
2025-12-09 01:23:36 +0100

    emacs: add commonlisp config

---
+++ .config/doom/dd/common-lisp.el
@@ -0,0 +1,13 @@
+;;; common-lisp.el -*- lexical-binding: t; -*-
+
+(after! sly
+  (setq sly-complete-symbol-function #'sly-flex-completions)
+
+  (setq sly-lisp-implementations
+        '((sbcl ("sbcl") :coding-system utf-8-unix)
+          (qlot ("qlot" "exec" "sbcl") :coding-system utf-8-unix)))
+
+  (setf (alist-get 'sly--external-completion completion-styles-alist)
+        '(sly--external-tryc
+          sly--external-allc
+          "Ad-hoc \"external completion\" style  (SLY flavor)")))
