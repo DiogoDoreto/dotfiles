6ef4545f998db455ef979773ec244eebd5b1641e
Diogo Doreto <diogo@doreto.com.br>
2026-02-06 12:25:22 +0100

    emacs: add some vpn transient commands

--- .config/doom/dd/host-lapdog.el
+++ .config/doom/dd/host-lapdog.el
@@ -30,3 +30,111 @@
       :desc "home-manager switch" :n "h" (cmd! (dd--make-nix "make hm-dog-lapdog-switch"))
       :leader :prefix ("C-SPC s" . "  SSH")
       :desc "Chungus dotfiles" :n "c" (cmd! (find-file "/sshx:chungus:p/dotfiles/")))
+
+(defconst systemd-bus :system)
+(defconst systemd-service "org.freedesktop.systemd1")
+(defconst systemd-path "/org/freedesktop/systemd1")
+(defconst systemd-interface "org.freedesktop.systemd1.Manager")
+
+(defun systemd-start-unit (unit-name)
+  "Start a systemd unit (e.g., nginx.service)."
+  (shell-command (format "systemctl start %s" unit-name)))
+
+(defun systemd-stop-unit (unit-name)
+  "Stop a systemd unit."
+  (shell-command (format "systemctl stop %s" unit-name)))
+
+(defun systemd-restart-unit (unit-name)
+  "Restart a systemd unit."
+  (shell-command (format "systemctl restart %s" unit-name)))
+
+(defun systemd-get-unit-path (unit-name)
+  "Get unit path, loading it if necessary."
+  (condition-case nil
+      (dbus-call-method systemd-bus
+                        systemd-service
+                        systemd-path
+                        systemd-interface
+                        "GetUnit"
+                        unit-name)
+    (dbus-error
+     ;; Fall back to LoadUnit
+     (dbus-call-method systemd-bus
+                       systemd-service
+                       systemd-path
+                       systemd-interface
+                       "LoadUnit"
+                       unit-name))))
+
+;; (systemd-get-unit-path (car systemd-transient-units))
+
+(defun systemd-unit-get-state (unit-name)
+  "Get the active state of a unit (active, inactive, failed, etc.)."
+  (dbus-get-property systemd-bus
+                     systemd-service
+                     (systemd-get-unit-path unit-name)
+                     "org.freedesktop.systemd1.Unit"
+                     "ActiveState"))
+
+(defvar systemd-transient-units
+  '("openvpn-vpn-es.service"
+    "openvpn-vpn-br.service")
+  "List of systemd units to control.")
+
+(defvar systemd-transient-current-unit (car systemd-transient-units)
+  "Currently selected unit.")
+
+(transient-define-prefix systemd-multi-unit-menu ()
+  "Control multiple systemd units."
+  [:description
+   (lambda ()
+     (let* ((state (systemd-unit-get-state systemd-transient-current-unit))
+            (state-face (pcase state
+                          ("active" 'success)
+                          ("inactive" 'warning)
+                          ("failed" 'error)
+                          (_ 'default)))
+            (state-indicator (pcase state
+                               ("active" "●")
+                               ("inactive" "○")
+                               ("failed" "✗")
+                               (_ "?"))))
+       (format "%s %s: %s"
+               (propertize state-indicator 'face state-face)
+               (propertize systemd-transient-current-unit 'face 'bold)
+               (propertize state 'face state-face))))
+   ["Actions"
+    ("s" "Start"
+     (lambda () (interactive)
+       (systemd-start-unit systemd-transient-current-unit))
+     :if-not (lambda () (equal (systemd-unit-get-state systemd-transient-current-unit) "active")))
+    ("S" "Stop"
+     (lambda () (interactive)
+       (systemd-stop-unit systemd-transient-current-unit))
+     :if (lambda () (equal (systemd-unit-get-state systemd-transient-current-unit) "active")))
+    ("r" "Restart"
+     (lambda () (interactive)
+       (systemd-restart-unit systemd-transient-current-unit))
+     :if (lambda () (equal (systemd-unit-get-state systemd-transient-current-unit) "active")))]
+   ["Unit Selection"
+    ("n" "Next unit"
+     (lambda ()
+       (interactive)
+       (let* ((units systemd-transient-units)
+              (current systemd-transient-current-unit)
+              (idx (cl-position current units :test 'equal))
+              (next-idx (mod (1+ idx) (length units))))
+         (setq systemd-transient-current-unit (nth next-idx units))
+         (transient-setup 'systemd-multi-unit-menu))))
+    ("u" "Choose unit"
+     (lambda ()
+       (interactive)
+       (setq systemd-transient-current-unit
+             (completing-read "Unit: " systemd-transient-units))
+       (transient-setup 'systemd-multi-unit-menu)))]
+   ["Other"
+    ("g" "Refresh" (lambda () (interactive) (transient-setup 'systemd-multi-unit-menu)))
+    ("q" "Quit" transient-quit-one)]]
+  (interactive)
+  (require 'dbus)
+  (transient-setup 'systemd-multi-unit-menu))
