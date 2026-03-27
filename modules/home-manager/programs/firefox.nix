fb172724b44e59e0c7d6f3e73bd702953586357c
Diogo Doreto <diogo@doreto.com.br>
2026-03-27 19:08:48 +0100

    add firefox dev-edition

--- modules/home-manager/programs/firefox.nix
+++ modules/home-manager/programs/firefox.nix
@@ -16,6 +16,10 @@ in
   };
 
   config = mkIf cfg.enable {
+    home.packages = [
+      pkgs.firefox-devedition
+    ];
+
     home.file = {
       "bin/d-firefox-devtools-fix".source = pkgs.writeShellScript "d-firefox-devtools-fix" ''
         # based on https://github.com/nix-community/home-manager/issues/6083#issuecomment-2642454543
@@ -23,103 +27,111 @@ in
       '';
     };
 
-    programs.firefox = {
-      enable = true;
-      profiles.dog = {
-        id = 0;
-        search = {
-          force = true;
-          default = "searxng";
-          privateDefault = "searxng";
-          engines = {
-            searxng = {
-              name = "SearXNG";
-              definedAliases = [ "@s" ];
-              urls = [
-                {
-                  template = "https://search.local.doreto.com.br";
-                  method = "POST";
-                  params = [
-                    {
-                      name = "q";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
+    programs.firefox =
+      let
+        base-profile = {
+          search = {
+            force = true;
+            default = "searxng";
+            privateDefault = "searxng";
+            engines = {
+              searxng = {
+                name = "SearXNG";
+                definedAliases = [ "@s" ];
+                urls = [
+                  {
+                    template = "https://search.local.doreto.com.br";
+                    method = "POST";
+                    params = [
+                      {
+                        name = "q";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
+              };
             };
           };
+          containersForce = true;
+          containers = {
+            Default = {
+              id = 0;
+              color = "toolbar";
+              icon = "circle";
+            };
+            Google = {
+              id = 1;
+              color = "red";
+              icon = "fence";
+            };
+            Shopping = {
+              id = 2;
+              color = "green";
+              icon = "dollar";
+            };
+            Facebook = {
+              id = 10;
+              color = "blue";
+              icon = "fence";
+            };
+          };
+          # See https://nur.nix-community.org/repos/rycee/
+          extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+            multi-account-containers
+            privacy-badger
+            raindropio
+            sidebery
+            ublock-origin
+            vimium
+          ];
+          # updates ~/.mozilla/firefox/dog/prefs.js
+          settings = {
+            "app.shield.optoutstudies.enabled" = false;
+            "browser.aboutConfig.showWarning" = false;
+            "browser.aboutwelcome.didSeeFinalScreen" = true;
+            "browser.ctrlTab.sortByRecentlyUsed" = true;
+            "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+            "browser.newtabpage.blocked" =
+              "{\"mZmevP23jfB3rScn/QCWnw==\":1,\"26UbzFJ7qT9/4DhodHKA1Q==\":1,\"4gPpjkxgZzXPVtuEoAL9Ig==\":1,\"eV8/WsSLxHadrTL1gAxhug==\":1,\"gLv0ja2RYVgxKdp0I5qwvA==\":1,\"T9nJot5PurhJSy8n038xGA==\":1}";
+            "browser.newtabpage.pinned" =
+              "[{\"url\":\"https://search.nixos.org/packages?channel=unstable&\",\"label\":\"Nix Packages\"}]";
+            "browser.places.importBookmarksHTML" = false;
+            "browser.uiCustomization.state" =
+              "{\"placements\":{\"widget-overflow-fixed-list\":[\"save-to-pocket-button\",\"fxa-toolbar-menu-button\"],\"unified-extensions-area\":[\"_testpilot-containers-browser-action\",\"_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"jid1-mnnxcxisbpnsxq_jetpack-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"urlbar-container\",\"customizableui-special-spring2\",\"downloads-button\",\"unified-extensions-button\",\"jid0-adyhmvsp91nuo8prv0mn2vkeb84_jetpack-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"vertical-tabs\":[],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\",\"_testpilot-containers-browser-action\",\"jid1-mnnxcxisbpnsxq_jetpack-browser-action\",\"jid0-adyhmvsp91nuo8prv0mn2vkeb84_jetpack-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"unified-extensions-area\",\"toolbar-menubar\",\"TabsToolbar\",\"widget-overflow-fixed-list\",\"vertical-tabs\"],\"currentVersion\":20,\"newElementCount\":2}";
+            "browser.urlbar.showSearchSuggestionsFirst" = false;
+            "datareporting.healthreport.uploadEnabled" = false;
+            "extensions.formautofill.creditCards.enabled" = false;
+            "font.name.monospace.x-western" = "VictorMono Nerd Font Mono";
+            "media.eme.enabled" = true;
+            "privacy.donottrackheader.enabled" = true;
+            "privacy.globalprivacycontrol.enabled" = true;
+            "signon.rememberSignons" = false;
+            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+            "widget.gtk.overlay-scrollbars.enabled" = true;
+          };
+          userChrome = ''
+            #TabsToolbar{ visibility: collapse !important }
+          '';
+          # userChrome = let
+          #   cssHacks = pkgs.fetchFromGitHub {
+          #     owner = "MrOtherGuy";
+          #     repo = "firefox-csshacks";
+          #     rev = "ec1aede46c2640d4daff491539c2637e27b07bdc";
+          #     sha256 = "n/hYUV8HIzf76S1v06OsVhft2J+jYUBbN53UwsQxzGw=";
+          #   };
+          # in readFile "${cssHacks}/chrome/hide_tabs_with_one_tab.css";
         };
-        containersForce = true;
-        containers = {
-          Default = {
-            id = 0;
-            color = "toolbar";
-            icon = "circle";
-          };
-          Google = {
-            id = 1;
-            color = "red";
-            icon = "fence";
-          };
-          Shopping = {
-            id = 2;
-            color = "green";
-            icon = "dollar";
-          };
-          Facebook = {
-            id = 10;
-            color = "blue";
-            icon = "fence";
-          };
+      in
+      {
+        enable = true;
+        profiles.dog = base-profile // {
+          id = 0;
         };
-        # See https://nur.nix-community.org/repos/rycee/
-        extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-          multi-account-containers
-          privacy-badger
-          raindropio
-          sidebery
-          ublock-origin
-          vimium
-        ];
-        # updates ~/.mozilla/firefox/dog/prefs.js
-        settings = {
-          "app.shield.optoutstudies.enabled" = false;
-          "browser.aboutConfig.showWarning" = false;
-          "browser.aboutwelcome.didSeeFinalScreen" = true;
-          "browser.ctrlTab.sortByRecentlyUsed" = true;
-          "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
-          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-          "browser.newtabpage.blocked" =
-            "{\"mZmevP23jfB3rScn/QCWnw==\":1,\"26UbzFJ7qT9/4DhodHKA1Q==\":1,\"4gPpjkxgZzXPVtuEoAL9Ig==\":1,\"eV8/WsSLxHadrTL1gAxhug==\":1,\"gLv0ja2RYVgxKdp0I5qwvA==\":1,\"T9nJot5PurhJSy8n038xGA==\":1}";
-          "browser.newtabpage.pinned" =
-            "[{\"url\":\"https://search.nixos.org/packages?channel=unstable&\",\"label\":\"Nix Packages\"}]";
-          "browser.places.importBookmarksHTML" = false;
-          "browser.uiCustomization.state" =
-            "{\"placements\":{\"widget-overflow-fixed-list\":[\"save-to-pocket-button\",\"fxa-toolbar-menu-button\"],\"unified-extensions-area\":[\"_testpilot-containers-browser-action\",\"_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"jid1-mnnxcxisbpnsxq_jetpack-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"urlbar-container\",\"customizableui-special-spring2\",\"downloads-button\",\"unified-extensions-button\",\"jid0-adyhmvsp91nuo8prv0mn2vkeb84_jetpack-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"vertical-tabs\":[],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\",\"_testpilot-containers-browser-action\",\"jid1-mnnxcxisbpnsxq_jetpack-browser-action\",\"jid0-adyhmvsp91nuo8prv0mn2vkeb84_jetpack-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_d7742d87-e61d-4b78-b8a1-b469842139fa_-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"unified-extensions-area\",\"toolbar-menubar\",\"TabsToolbar\",\"widget-overflow-fixed-list\",\"vertical-tabs\"],\"currentVersion\":20,\"newElementCount\":2}";
-          "browser.urlbar.showSearchSuggestionsFirst" = false;
-          "datareporting.healthreport.uploadEnabled" = false;
-          "extensions.formautofill.creditCards.enabled" = false;
-          "font.name.monospace.x-western" = "VictorMono Nerd Font Mono";
-          "media.eme.enabled" = true;
-          "privacy.donottrackheader.enabled" = true;
-          "privacy.globalprivacycontrol.enabled" = true;
-          "signon.rememberSignons" = false;
-          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-          "widget.gtk.overlay-scrollbars.enabled" = true;
+        profiles.dev-edition-default = base-profile // {
+          id = 1;
         };
-        userChrome = ''
-          #TabsToolbar{ visibility: collapse !important }
-        '';
-        # userChrome = let
-        #   cssHacks = pkgs.fetchFromGitHub {
-        #     owner = "MrOtherGuy";
-        #     repo = "firefox-csshacks";
-        #     rev = "ec1aede46c2640d4daff491539c2637e27b07bdc";
-        #     sha256 = "n/hYUV8HIzf76S1v06OsVhft2J+jYUBbN53UwsQxzGw=";
-        #   };
-        # in readFile "${cssHacks}/chrome/hide_tabs_with_one_tab.css";
       };
-    };
   };
 }
