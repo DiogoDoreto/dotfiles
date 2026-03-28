b5c0c6913ce3f3b03d0bbdc193dc2015e4d78697
Diogo Doreto <diogo@doreto.com.br>
2026-03-28 21:17:25 +0100

    chungus: add nix-community cache

--- hosts/chungus/configuration.nix
+++ hosts/chungus/configuration.nix
@@ -11,6 +11,15 @@
     ./services/llama-swap.nix
   ];
 
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   hardware.nvidia-container-toolkit = {
     enable = true;
     mount-nvidia-executables = false;
