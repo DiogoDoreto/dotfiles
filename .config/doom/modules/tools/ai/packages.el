cd97abe518dd0aed5f69b2cadc4155c07689a09d
Diogo Doreto <diogo@doreto.com.br>
2026-04-22 00:26:14 +0200

    ❄️

--- .config/doom/modules/tools/ai/packages.el
+++ .config/doom/modules/tools/ai/packages.el
@@ -1,12 +1,12 @@
 ;; -*- lexical-binding: t; no-byte-compile: t; -*-
 
 (package! gptel :recipe (:nonrecursive t)
-  :pin "8d6411b5f89d796c817ff79324973b8910e164fe")
+  :pin "a0adf16a3c9f1aadcb3ed92b76be371f2e5b5219")
 
 (package! mcp :pin "5c105a8db470eb9777fdbd26251548dec42c03f0")
 
 (package! shell-maker :pin "6377cbdb49248d670170f1c8dbe045648063583e")
 (package! acp :pin "c32fbf8df34ed0095853a8cf55dc783e68b67d90")
-(package! agent-shell :pin "7f106a355295d6c0fde5cd589cf566df7850463f")
+(package! agent-shell :pin "023ad9caaf9b66162d58c6189e61622ee95d13c8")
 
 (package! copilot :pin "ab5c58bc969f52f6d75e972658f2c3381c70b4fa")
