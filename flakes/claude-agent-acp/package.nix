8439bfa0a9baa3988404772fdd0e20ccd4a97461
Diogo Doreto <diogo@doreto.com.br>
2026-04-25 11:43:50 +0200

    update claude-agent-acp to v0.31.0

--- flakes/claude-agent-acp/package.nix
+++ flakes/claude-agent-acp/package.nix
@@ -6,16 +6,16 @@
 
 buildNpmPackage rec {
   pname = "claude-agent-acp";
-  version = "0.30.0";
+  version = "0.31.0";
 
   src = fetchFromGitHub {
     owner = "agentclientprotocol";
     repo = "claude-agent-acp";
     rev = "v${version}";
-    hash = "sha256-Fb5P9LUPIeVYZ7LDVreHZCtuXUtHNdZjqC4gRVGVg50=";
+    hash = "sha256-lWAuf8EO5Y1x1HhcNrbNQUgOsdGG5SXYTiXevWBEhSQ=";
   };
 
-  npmDepsHash = "sha256-lF1me4oRLCol2Nx14BWjognjmzK6GzHZJajS6s4tJSQ=";
+  npmDepsHash = "sha256-lKGj7J9UduqfVPUiYh+ZcCqZ8tfzmV4mVI2dGSZWj0Q=";
 
   npmPackFlags = [ "--ignore-scripts" ];
 
