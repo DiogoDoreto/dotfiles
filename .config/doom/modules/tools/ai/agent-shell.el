90a8000ee920b8abad6733af0841a9ff59c85537
Diogo Doreto <diogo@doreto.com.br>
2026-04-27 16:36:04 +0200

    emacs(agent-shell): improve configs

--- .config/doom/modules/tools/ai/agent-shell.el
+++ .config/doom/modules/tools/ai/agent-shell.el
@@ -4,16 +4,19 @@
   :defer t
   :after acp
   :config
-  (setq agent-shell-session-strategy 'prompt
-        agent-shell-show-usage-at-turn-end t)
+  (setopt agent-shell-session-strategy 'prompt
+          agent-shell-show-usage-at-turn-end t
+          agent-shell-context-sources '(files region error)
+          agent-shell-agent-configs (list (agent-shell-anthropic-make-claude-code-config)
+                                          (agent-shell-opencode-make-agent-config)))
 
   (when (string= (system-name) "lapdog")
-    (setopt agent-shell-preferred-agent-config (agent-shell-anthropic-make-claude-code-config)
+    (setopt agent-shell-preferred-agent-config (car agent-shell-agent-configs)
             ;; env var fixes init issue https://github.com/agentclientprotocol/claude-agent-acp/issues/575
             agent-shell-anthropic-claude-acp-command '("ssh" "lapdog-agent" "CLAUDE_CODE_EXECUTABLE=/etc/profiles/per-user/dog/bin/claude" "claude-agent-acp")))
 
   (when (string= (system-name) "DT-5RHWB24")
-    (setopt agent-shell-preferred-agent-config (agent-shell-opencode-make-agent-config)))
+    (setopt agent-shell-opencode-acp-command '("opencode" "acp" "--attach" "http://localhost:4242")))
 
   ;; from https://github.com/xenodium/agent-shell/issues/259
   (add-hook 'agent-shell-mode-hook (lambda () (setq-local doom-real-buffer-p t)))
