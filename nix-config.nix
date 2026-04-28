69ea8c7ccfa0ccae6b048ce4f55e90ea5ae1a308
Diogo Doreto <diogo@doreto.com.br>
2026-04-28 08:42:50 +0200

    update nix-config.nix file to support home-manager only systems

--- nix-config.nix
+++ nix-config.nix
@@ -1,21 +1,51 @@
 inputs:
-{ lib, ... }:
 {
-  nix.registry = lib.mapAttrs (_: flake: { inherit flake; }) (
-    lib.filterAttrs (_: v: v ? outputs) inputs
-  );
-  nix.channel.enable = false;
-  # but NIX_PATH is still used by many useful tools, so we set it to the same value as the one used by this flake.
-  # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
-  environment.etc = lib.mapAttrs' (name: flake: {
-    name = "nix/inputs/${name}";
-    value.source = "${flake}";
-  }) inputs;
-  # https://github.com/NixOS/nix/issues/9574
-  nix.settings.nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
+  lib,
+  config,
+  options,
+  pkgs,
+  ...
+}:
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+let
+  isNixOS = options ? environment;
+  filteredInputs = lib.filterAttrs (_: v: v ? outputs) inputs;
+in
+{
+  config = lib.mkMerge (
+    [
+      {
+        nix.registry = lib.mapAttrs (_: flake: { inherit flake; }) filteredInputs;
+
+        # but NIX_PATH is still used by many useful tools, so we set it to the same value as the one used by this flake.
+        # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
+        # https://github.com/NixOS/nix/issues/9574
+        nix.settings.nix-path = lib.mkForce (
+          if isNixOS then
+            "nixpkgs=/etc/nix/inputs/nixpkgs"
+          else
+            "nixpkgs=${config.home.homeDirectory}/.config/nix/inputs/nixpkgs"
+        );
+
+        nix.settings.experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+      }
+    ]
+    ++ lib.optional isNixOS {
+      nix.channel.enable = false;
+      environment.etc = lib.mapAttrs' (name: flake: {
+        name = "nix/inputs/${name}";
+        value.source = "${flake}";
+      }) inputs;
+    }
+    ++ lib.optional (!isNixOS) {
+      nix.package = pkgs.nix;
+      home.file = lib.mapAttrs' (name: flake: {
+        name = ".config/nix/inputs/${name}";
+        value.source = "${flake}";
+      }) filteredInputs;
+    }
+  );
 }
