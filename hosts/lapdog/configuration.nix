3b33ed1088c5fd6c375f8f4954729fccf59f1f32
Diogo Doreto <diogo@doreto.com.br>
2026-05-16 19:52:55 +0200

    lapdog: use plasma login manager

--- hosts/lapdog/configuration.nix
+++ hosts/lapdog/configuration.nix
@@ -119,7 +119,7 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  services.displayManager.plasma-login-manager.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
