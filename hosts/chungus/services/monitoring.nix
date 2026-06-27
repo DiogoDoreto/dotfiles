cf4cbc9e77ddf7e8bc8c19fa9a131427b500d26e
Diogo Doreto <diogo@doreto.com.br>
2026-06-27 13:19:17 +0000

    monitoring: ship chungus journal with Fluent Bit

--- hosts/chungus/services/monitoring.nix
+++ hosts/chungus/services/monitoring.nix
@@ -37,6 +37,13 @@ in
     port = nvidiaExporterPort;
   };
 
+  dog.services.journald-to-victorialogs = {
+    enable = true;
+    host = "logs.local.doreto.com.br";
+    port = 443;
+    tls = true;
+  };
+
   # Allow only mini (192.168.0.2) to scrape the exporters on this host.
   networking.firewall.extraCommands = ''
     iptables -A nixos-fw -p tcp -s 192.168.0.2 --dport ${toString nodeExporterPort} -j nixos-fw-accept
