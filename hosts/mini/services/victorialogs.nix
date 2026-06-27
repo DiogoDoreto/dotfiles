0ce61301b0381ed5fef68c29090eb19d73e8cbe8
Diogo Doreto <diogo@doreto.com.br>
2026-06-27 13:13:58 +0000

    monitoring: ship mini journal with Fluent Bit

--- hosts/mini/services/victorialogs.nix
+++ hosts/mini/services/victorialogs.nix
@@ -13,4 +13,11 @@ in
       "-retentionPeriod=3" # months
     ];
   };
+
+  dog.services.journald-to-victorialogs = {
+    enable = true;
+    host = "127.0.0.1";
+    port = vars.ports.victorialogs;
+    tls = false;
+  };
 }
