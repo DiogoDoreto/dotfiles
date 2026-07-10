79fde34cbdf9eeb3e2f3edc28a8d2c7f85f0cf0d
Diogo Doreto <diogo@doreto.com.br>
2026-07-10 15:49:26 +0200

    mini(cloudflared): update tunnel id

--- hosts/mini/services/cloudflared.nix
+++ hosts/mini/services/cloudflared.nix
@@ -8,7 +8,9 @@ in
 {
   services.cloudflared = {
     enable = true;
-    tunnels."ed93f0fa-f178-4219-8b08-070240906058" = {
+    # nix run nixpkgs#cloudflared -- tunnel create doreto.com.br
+    # cp .cloudflared/a31719a0-f44c-4471-be04-10a689f6bcd5.json /etc/secrets/cloudflared/doreto-tunnel.json
+    tunnels."a31719a0-f44c-4471-be04-10a689f6bcd5" = {
       credentialsFile = "/etc/secrets/cloudflared/doreto-tunnel.json";
       ingress = {
         "*.doreto.com.br" = "http://127.0.0.1:${p.publicCaddy}";
