14080ef0c42d338b9ae1a1e3b1de936bb0df9c7a
Diogo Doreto <diogo@doreto.com.br>
2026-08-26 14:49:37 +0200

    emacs(agent-shell): update packages

--- .config/doom/modules/tools/ai/packages.el
+++ .config/doom/modules/tools/ai/packages.el
@@ -4,8 +4,8 @@
   :pin "99f0438bfb50e8df3707392e08194a12764bc737")
 (package! gptel-agent :pin "e833bcaf617baf8c8075eac098231c4457386814")
 
-(package! shell-maker :pin "071c6df3ca22a2f4c0daa689848ac9bd21bf4e2b")
-(package! acp :pin "a29cb161ac95f1819f34481a98666707661c5cf8")
-(package! agent-shell :pin "e82b4fea6c3790b57bb6071fbf9aad365163c153")
+(package! shell-maker :pin "ab4f8ebaf4ef7a7db4762c5d5075baea580044ba")
+(package! acp :pin "4d7d58dc39870e9390e94617e13d7ada175d7945")
+(package! agent-shell :pin "0fe9ed04ee20a85e098425fe59ae17e93d4a974f")
 
 (package! copilot :pin "277ca357422ba34bcf7fe650cb720580994eea84")
